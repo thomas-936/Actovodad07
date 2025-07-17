6ed67913dcd3a0ef758f4972980180b08936f722
tomas <tomas27@gmail.com>
2025-07-16 19:29:28 -0600

    tercer commit

--- Actividad_07.py
+++ Actividad_07.py
@@ -16,6 +16,39 @@ while opcion!= 4:
             for i in range(cantidad):
                 print(f"\nIngrese estudiante #{i}")
                 while True:
+                    carnet = input("Ingrese el carnet del estudiante: ")
+                    if carnet in estudiantes:
+                        print("El carnet ya fue registrado, ingrese uno nuevo... ")
+                    else:
+                        break
+            nombre = input("Imngrese el nombre del estudiante: ")
+            edad = int(input("Ingrese la edad del estudiante: "))
+            carrera = input("Ingrese la carrera del estudiante: ")
+            cantidad_cursos = int(input("Ingrese la cantidad de cursoso: "))
+            for j in range(cantidad_cursos):
+                print(f"Ingrese el curso #{j}: ")
+                while True:
+                    codigo_curso = input("Ingrese el codigo del curso: ")
+                    if codigo_curso in estudiantes:
+                        print("Este codigo ya esta registrado, intentelo de nuevo... ")
+                    else :
+                        break
+            nota_tareas = int(input("Ingrese la nota de las tareas: "))
+            if nota_tareas > 0 and nota_tareas < 100:
+                print("Nota ingresada...")
+            else:
+                print("Ingrese una nota correcta.")
+            nota_parciales = int(input("Ingrese la nota de los parciales: "))
+            if nota_parciales > 0 and nota_parciales < 100:
+                print("Nota ingresada... ")
+            else:
+                print("Ingrese una nota correcta.")
+            nota_proyecto = int(input("Ingrese la nota del proyecto: "))
+            if nota_proyecto > 0 and nota_proyecto < 100:
+                print("Nota ingresada...")
+            else:
+                print("Ingrese una nota correcta.")
+
 
 
 
