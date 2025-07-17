83c991906912cbf44baec0cd7b3e3764ee7be959
tomas <tomas27@gmail.com>
2025-07-16 19:41:27 -0600

    tercer commit

--- Actividad_07.py
+++ Actividad_07.py
@@ -21,33 +21,48 @@ while opcion!= 4:
                         print("El carnet ya fue registrado, ingrese uno nuevo... ")
                     else:
                         break
-            nombre = input("Imngrese el nombre del estudiante: ")
-            edad = int(input("Ingrese la edad del estudiante: "))
-            carrera = input("Ingrese la carrera del estudiante: ")
-            cantidad_cursos = int(input("Ingrese la cantidad de cursoso: "))
-            for j in range(cantidad_cursos):
-                print(f"Ingrese el curso #{j}: ")
-                while True:
-                    codigo_curso = input("Ingrese el codigo del curso: ")
-                    if codigo_curso in estudiantes:
-                        print("Este codigo ya esta registrado, intentelo de nuevo... ")
-                    else :
-                        break
-            nota_tareas = int(input("Ingrese la nota de las tareas: "))
-            if nota_tareas > 0 and nota_tareas < 100:
-                print("Nota ingresada...")
-            else:
-                print("Ingrese una nota correcta.")
-            nota_parciales = int(input("Ingrese la nota de los parciales: "))
-            if nota_parciales > 0 and nota_parciales < 100:
-                print("Nota ingresada... ")
-            else:
-                print("Ingrese una nota correcta.")
-            nota_proyecto = int(input("Ingrese la nota del proyecto: "))
-            if nota_proyecto > 0 and nota_proyecto < 100:
-                print("Nota ingresada...")
-            else:
-                print("Ingrese una nota correcta.")
+                nombre = input("Ingrese el nombre del estudiante: ")
+                edad = int(input("Ingrese la edad del estudiante: "))
+                carrera = input("Ingrese la carrera del estudiante: ")
+                cantidad_cursos = int(input("Ingrese la cantidad de cursoso: "))
+                for j in range(cantidad_cursos):
+                    print(f"Ingrese el curso #{j}: ")
+                    while True:
+                        codigo_curso = input("Ingrese el codigo del curso: ")
+                        if codigo_curso in estudiantes:
+                            print("Este codigo ya esta registrado, intentelo de nuevo... ")
+                        else :
+                            break
+                nota_tareas = int(input("Ingrese la nota de las tareas: "))
+                if nota_tareas > 0 and nota_tareas < 100:
+                    print("Nota ingresada...")
+                else:
+                    print("Ingrese una nota correcta.")
+                nota_parciales = int(input("Ingrese la nota de los parciales: "))
+                if nota_parciales > 0 and nota_parciales < 100:
+                    print("Nota ingresada... ")
+                else:
+                    print("Ingrese una nota correcta.")
+                nota_proyecto = int(input("Ingrese la nota del proyecto: "))
+                if nota_proyecto > 0 and nota_proyecto < 100:
+                    print("Nota ingresada...")
+                else:
+                    print("Ingrese una nota correcta.")
+
+            estudiantes[carnet] = {
+                "nombre": nombre,
+                "edad": edad,
+                "carrera": carrera,
+                "codigo_curso":{
+                    "nota_tareas": nota_tareas,
+                    "nota_parciales": nota_parciales,
+                    "nota_proyecto": nota_proyecto
+                }
+
+            }
+
+
+
 
 
 
